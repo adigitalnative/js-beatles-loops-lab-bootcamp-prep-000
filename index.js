471978b20b3052ad301a85583eb769d0d1e50992
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 15:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function theBeatlesPlay(musicians, instruments) {
   const array = []
   
   for(let i=0, len = musicians.length; i < len; i++) {
-    array.push(`${musicians[1]} plays ${instruments[1]}`)
+    array.push(`${musicians.shift()} plays ${instruments[1]}`)
+    musicians.
   }
   
   return array
